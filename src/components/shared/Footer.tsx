455026a3e5ad4c00714f834910b378ed78a0a13e
barakadanny <barakadan421@gmail.com>
2024-10-04 18:01:37 +0100

    Footer component

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100 dark:bg-gray-800 text-black dark:text-white transition-colors duration-200">
+      <div className="px-4 mx-auto sm:px-6 lg:px-8">
+        <div className="py-4">
+          <p>This is the Footer</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
